1a0263db86c66a20dc4f32cac9aa78d144dc58ad
Yuqiu Wang <teresa@Yuqius-MacBook-Air.local>
2020-04-08 14:56:32 -0400

    adds a python scriptthat prints a message greeting Github

---
+++ python_code_hello_github.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Coursera_ Capstone
+
+# ###### This notebook will be used for the capstone project.
+# 
+
+# In[3]:
+
+
+import pandas as pd
+
+
+# In[4]:
+
+
+import numpy as np
+
+
+# In[5]:
+
+
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
